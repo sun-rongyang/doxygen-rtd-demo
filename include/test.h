dcf8c6b77c1b2b6a02aafff56b347b345c09d3af
Sun Rongyang <sun-rongyang@outlook.com>
2020-04-09 20:24:43 +0800

    Doxygen with ReadTheDocs test repo init

---
+++ include/test.h
@@ -0,0 +1,23 @@
+/*
+* Author: Rongyang Sun <sun-rongyang@outlook.com>
+* Creation Date: 2020-04-08 21:40
+* 
+* Description: File description
+*/
+
+
+/** A test class.
+ *
+ * This are the detail descriptions.
+ */
+class TestClass {
+public:
+  /** The 1st public member.
+   *
+   * I am the 1st public function member.
+   */
+  void PubFst(
+      int         ///< a int param
+  );
+
+};
---
+++ src/test.cc
@@ -0,0 +1,10 @@
+/** Implementation details.
+ */
+#include "test.h"
+
+
+void TestClass::PubFst(
+    int a ///< a int param.
+) {
+
+}
